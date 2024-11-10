3aa924cf015dd9f43d53d93a04044327b44e16f7
Shashank <shashankdevulapally569@gmail.com>
2024-11-10 05:30:02 +0530

    new file:   healthcare/backend/data.js 	modified:   healthcare/backend/request.rest 	modified:   healthcare/backend/routes/authRoutes.js 	modified:   healthcare/backend/server.js

--- healthcare/backend/routes/authRoutes.js
+++ healthcare/backend/routes/authRoutes.js
@@ -1,9 +1,30 @@
-// // routes/authRoutes.js
-// const express = require('express');
-// const { register, login } = require('../controllers/authController');
-// const router = express.Router();
+const express = require('express');
+const router = express.Router();
 
-// router.post('/register', register);
-// router.post('/login', login);
+const Therapist  = require("../models/Therapist");
+const Patient = require("../models/Patient");
 
-// module.exports = router;
+router.post('/register', async (req, res) => {
+    const { email, username, password, therapist_id } = req.body;
+    try {
+        const patient = new Patient({
+            username,
+            password,
+            therapist_id
+        });
+        await patient.save();
+
+        const therapist = await Therapist.findOne({ _id: therapist_id });
+        if (therapist) {
+            therapist.patients.push(patient._id);
+            await therapist.save();
+        }
+
+        res.status(201).json({ message: "Patient registered successfully" });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "Failed to register patient" });
+    }
+});
+
+module.exports = router;
